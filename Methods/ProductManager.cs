b24b9c403cca0d174c2ae7ce0358076c08add9c9
Feyza Ozcan <202003001079@dogus.edu.tr>
2021-01-17 15:59:09 +0300

    Add project files.

---
+++ Methods/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class ProductManager
+    {
+        //naming convention
+        //syntax
+        public void Add(Product product)
+        {
+            Console.WriteLine("Tebrikler, Sepete eklendi : " + product.Adi);
+        }
+
+        public void Add2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler, Sepete eklendi : " + urunAdi);
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyat = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Product product1 = new Product();
+            product1.Adi = "Elma";
+            product1.Fiyati = 15;
+            product1.Aciklama = "Amasya elması";
+
+            Product product2 = new Product();
+            product2.Adi = "Kapruz";
+            product2.Fiyati = 80;
+            product2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] products = new Product[] { product1, product2 };
+
+            // type-safe -- tip güvenliği
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Fiyati);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine("--------------");
+            }
+
+            Console.WriteLine("------Methods-----");
+            //instance - örnek
+            //encapsulation
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            productManager.Add(product2);
+
+            productManager.Add2("Armut", "Yeşil Armut", 12, 10);
+            productManager.Add2("Elma", "Yeşil Elma", 9, 9);
+            productManager.Add2("Karpuz", "Diyarbakır karpuzu", 19, 8);
+        }
+    }
+}
